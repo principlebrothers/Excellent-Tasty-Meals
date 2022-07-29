3a6352146649a6106ba50afbb7639a28be38e6d7
principlebrothers <principlesbrothers@yahoo.com>
2022-07-29 15:22:34 +0000

    Write test on the commentCounter function

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
 /**
  * @jest-environment jsdom
  */
+import apiInfo from "./__mock__/api";
+import {commentCounter} from './src/modules/populateUi'
+
+describe('Comment counter', () => {
+  document.body.innerHTML = `<h2 class="title">
+  Comments (<span class="comment-cout">0</span>)
+</h2>`;
+  const commentCount = document.querySelector('.comment-cout');
+  test('Count the number of comments to be four (4)', () => {
+    expect(commentCounter(apiInfo, commentCount)).toBe(4);
+  });
+
+});
